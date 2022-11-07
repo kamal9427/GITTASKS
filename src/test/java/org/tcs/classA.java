3ca028e8d510b164f05510af4b9fd0449191478e
kamal9427 <kamalakannan9427@gmail.com>
2022-11-07 15:05:10 +0530

    Codes updated

---
+++ src/test/java/org/tcs/classA.java
@@ -0,0 +1,19 @@
+package org.tcs;
+
+public class classA {
+	
+	public void bike() {
+		System.out.println("RE");
+	}
+	
+	public void flight() {
+		System.out.println("AirIndia");
+	}
+	
+	public void car() {
+		System.out.println("Audi");
+	}
+	
+	
+
+}
